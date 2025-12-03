4e2cf062c17b9ff7f15c172b09b979af778dea1d
Alex Chan <alex@alexwlchan.net>
2025-12-03 00:51:11 +0000

    Add a function for parsing Mastodon post URLs

--- src/chives/__init__.py
+++ src/chives/__init__.py
@@ -11,4 +11,4 @@ I share across multiple sites.
 
 """
 
-__version__ = "5"
+__version__ = "6"
--- src/chives/urls.py
+++ src/chives/urls.py
@@ -1,5 +1,8 @@
 """Code for manipulating and tidying URLs."""
 
+import re
+from typing import TypedDict
+
 import hyperlink
 
 
@@ -15,3 +18,28 @@ def clean_youtube_url(url: str) -> str:
     u = u.remove("t")
 
     return str(u)
+
+
+def parse_mastodon_post_url(url: str) -> tuple[str, str, str]:
+    """
+    Parse a Mastodon post URL into its component parts:
+    server, account, post ID.
+    """
+    u = hyperlink.parse(url)
+
+    if len(u.path) != 2:
+        raise ValueError("Cannot parse Mastodon URL!")
+
+    if not u.path[0].startswith("@"):
+        raise ValueError("Cannot find `acct` in Mastodon URL!")
+
+    if not re.fullmatch(r"^[0-9]+$", u.path[1]):
+        raise ValueError("Mastodon post ID is not numeric!")
+
+    if u.host == "social.alexwlchan.net":
+        _, acct, server = u.path[0].split("@")
+    else:
+        server = u.host
+        acct = u.path[0].replace("@", "")
+
+    return server, acct, u.path[1]
