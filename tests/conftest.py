54c956f839a796a274fca9c4bcf1cad22612d8c1
Alex Chan <alex@alexwlchan.net>
2026-04-26 23:33:53 +0100

    all: move the project from GitHub to alexwlchan.net

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-"""Shared helpers and test fixtures."""
-
-from cassettes import cassette_name, vcr_cassette
-
-pytest_plugins = "pytester"
-
-__all__ = ["cassette_name", "vcr_cassette"]
--- tests/stubs/smartypants.pyi
+++
@@ -1,8 +0,0 @@
-class Attr:
-    q: int
-    D: int
-    e: int
-    u: int
-    w: int
-
-def smartypants(text: str, attrs: int) -> str: ...
